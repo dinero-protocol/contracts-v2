be37912580270c42fc9bff3b858f7de7fe77acdf
dreahrealm <366129+drahrealm@users.noreply.github.com>
2022-04-22 11:00:44 +0700

    Add 0.7.5 support

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -52,6 +52,15 @@ const config: HardhatUserConfig = {
         },
       },
       {
+        version: '0.7.5',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
         version: '0.8.4',
         settings: {
           optimizer: {
