75de51904545ad2110863d7a25c6d4d186c65b85
Baby Yoda <baby.yoda.baby@protonmail.com>
2022-04-23 14:55:01 -0500

    Mariposa setup and vault set

--- test/its.ts
+++ test/its.ts
@@ -1,10 +1,7 @@
 import {expect} from "chai";
 import { Contract, Signer } from "ethers";
 
-export async function vaultIsMariposa(BTRFLY: Contract, signer: Signer, Mariposa_addr: string) :Promise<string>{
+export async function vaultIsMariposa(BTRFLY: Contract, signer: Signer, Mariposa_addr: string){
     const vault_addr = await BTRFLY.connect(signer).vault(); 
-
-    console.log(`the vault address for the btrfly contract is ${vault_addr}`);
-    expect(vault_addr).to.be.equals(Mariposa_addr);
-    return vault_addr;  
+    expect(vault_addr).to.be.equals(Mariposa_addr); 
 }
--- test/mocks/mockMariposa.ts
+++ test/mocks/mockMariposa.ts
@@ -6,15 +6,15 @@ import { ethers, network } from "hardhat";
  * Impersonates the multisig address
  * @returns impersonated ownerSigner
  */
- export async function impersonateSigner() :Promise<Signer> {
+ export async function impersonateSigner() {
     const ownerAddr = "0xA52Fd396891E7A74b641a2Cb1A6999Fcf56B077e";
     await network.provider.send('hardhat_impersonateAccount', [ownerAddr]);
 
     const ownerSigner: Signer = ethers.provider.getSigner(ownerAddr);
-    // const owner_addr = await ownerSigner.getAddress();
+    const owner_addr = await ownerSigner.getAddress();
 
-    // let balance: string = "0x10000000000000000000000";
-    // await network.provider.send("hardhat_setBalance", [owner_addr, balance, ]);
+    let balance: string = "0x10000000000000000000000";
+    await network.provider.send("hardhat_setBalance", [owner_addr, balance, ]);
 
     return ownerSigner;
 }
@@ -32,19 +32,17 @@ export async function setupMariposa(
     cap_of_tokens: string,
     duration_of_epoch: number,
     multisig_addr: string
-) :Promise<Contract>{
-
-
+) {
     // Gets the mariposa contract and deploys
     const mariposaFactory = await ethers.getContractFactory("Mariposa");
-   const Mariposa = await mariposaFactory.deploy(
+    const Mariposa = await mariposaFactory.deploy(
         btrfly_addr,
         cap_of_tokens,
         duration_of_epoch
     ); 
     
     // sets the owner of the Mariposa contract to be the multisig
-    await Mariposa.connect(Mariposa.address).transferOwnership(multisig_addr); 
+    await Mariposa.transferOwnership(multisig_addr); 
 
     // Prints the address of Mariposa contract that has been deployed 
     console.log(`\tDeployed Mariposa contract at ${Mariposa.address}`);
@@ -57,7 +55,7 @@ export async function setupMariposa(
  * @param signer 
  * @returns BTRFLYContract 
  */
-export async function setupBTRFLY(btrfly_addr: string, signer: Signer): Promise<Contract>{
+export async function setupBTRFLY(btrfly_addr: string, signer: Signer){
     const BTRFLYContract = await ethers.getContractAt("BTRFLY", btrfly_addr, signer);
     return BTRFLYContract; 
 }
