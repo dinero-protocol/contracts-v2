40da33bae99ce88d16acde3a9beb2dcdb6e7aeab
neverDefined <hk-nft@protonmail.com>
2022-05-10 14:57:27 +0300

    mariposa-simple-contract

---
+++ contracts/mock/Bond.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import {Mariposa} from "../core/Mariposa.sol";
+
+contract Bond {
+    Mariposa public immutable mariposa;
+
+    constructor(address _mariposa) {
+        require(_mariposa != address(0), "Bond: zero address");
+        mariposa = Mariposa(_mariposa);
+    }
+
+    function bond(uint256 _amount) external {
+        mariposa.request(address(this), _amount);
+    }
+}
