d0ba56c2a191f0a322a1fdd8ebf1173b7c096052
realkinando <realkinando@LAPTOP-QO2JA4V1.localdomain>
2022-05-13 20:07:30 +0100

    token contract

---
+++ contracts/core/BTRFLYV2.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import {VaultOwned} from "../olympusUtils/VaultOwned.sol";
+import {ERC20} from "@rari-capital/solmate/src/tokens/ERC20.sol";
+
+/// @title BTRFLYV2
+/// @author Realkinando
+
+/**
+    @notice 
+    Minimum viable token for BTRFLYV2, follows same patterns as V1 token, but with improved readability
+*/
+
+contract BTRFLYV2 is VaultOwned, ERC20("BTRFLY","BTRFLY",18) {
+
+    /**
+        @notice externally exposes the _mint method to the vault address
+        @param  to      address  address to receive tokens
+        @param  amount  uint256  amount to mint
+     */
+    function mint(address to, uint256 amount) external onlyVault{
+        _mint(to, amount);
+    }
+
+}
