071e21cde05bd663f1abf3323d5b9a4ecb2af54f
neverDefined <hk-nft@protonmail.com>
2022-08-08 12:37:46 +0100

    set-final-params-in-constants

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,6 +1,11 @@
+import { ethers } from 'ethers';
 export const multisigAddress = '0xA52Fd396891E7A74b641a2Cb1A6999Fcf56B077e';
 export const btrflyAddress = '0xC0d4Ceb216B3BA9C3701B291766fDCbA977ceC3A';
 export const xBtrflyAddress = '0xCC94Faf235cC5D3Bf4bEd3a30db5984306c86aBC';
 export const wxBtrflyAddress = '0x4B16d95dDF1AE4Fe8227ed7B7E80CF13275e61c9';
 export const stakingAddress = '0xBdE4Dfb0dbb0Dd8833eFb6C5BD0Ce048C852C487';
 export const distributorAddress = '0xB2120AE79d838d6703Cf6d2ac5cC68b5DB10683F';
+export const mariposaCap = ethers.utils.parseEther((650_000).toString()); // 650,000 in 1e18
+export const totalSupplyOfV1inV2 = ethers.utils.parseEther(
+  (145731.742331065).toString()
+);
