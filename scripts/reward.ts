c64a69560334130675af164e7d1d597fc5079d98
dreahrealm <366129+drahrealm@users.noreply.github.com>
2023-04-18 09:10:56 +0700

    Update reward.ts

--- scripts/reward.ts
+++ scripts/reward.ts
@@ -10,7 +10,7 @@ import {
   Multicall,
   Multicall__factory,
   RLBTRFLY,
-  RewardDistributor,
+  // RewardDistributor,
 } from '../typechain';
 import { setUserRewards } from './helpers/redis';
 
@@ -65,25 +65,34 @@ const providerUrl =
   process.env.NODE_ENV !== 'production' ? GOERLI_URL : MAINNET_URL;
 const provider = new ethers.providers.StaticJsonRpcProvider(providerUrl);
 
-// TODO: Should replace with mainnet values on production
 const rlBtrflyAddress = '0x742B70151cd3Bc7ab598aAFF1d54B90c3ebC6027';
 const multicallAddress = '0xeefba1e63905ef1d7acba5a8513c70307c1ce441';
 const rewardDistributorAddress = '0xd7807E5752B368A6a64b76828Aaff0750522a76E';
-const minBlock = 15317426; // Block for the locked balance snapshot
-const maxBlock = 15361719; // Block for the relocked balance snapshot
-const deadline = 1660176000; // Epoch timestamp for the current snapshot
-const previousDeadline = 0; // Epoch timestamp for the previous snapshot
+const btrflyV2Address = '0xc55126051b22ebb829d00368f4b12bde432de5da';
+const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+
+// Updated for epoch #17
+const curBlock = 16886553; // Block for the locked balance snapshot
+const minBlock = 16836711; // Block for the locked balance snapshot (1 week before deadline)
+const maxBlock = 16936361; // Block for the relocked balance snapshot (1 week after deadline)
+const deadline = 1679529600; // Epoch timestamp for the current snapshot
+const previousDeadline = deadline - 1209600; // Epoch timestamp for the previous snapshot
+const previousBlock = 16786948; // 2 weeks before curBlock and before midnight
 
 // Used for storing cached distribution data
-const dataDir = `data`;
-const currentDir = `${dataDir}/${deadline}`;
-const previousDir = `${dataDir}/${previousDeadline}`;
+const distDir = `data/dist`;
+const snapshotDir = `data/snapshot`;
+const currentDistDir = `${distDir}/${deadline}`;
+const previousDistDir = `${distDir}/${previousDeadline}`;
+const currentSnapshotDir = `${snapshotDir}`;
 
 // Replace with list of tx hashes used for sending in rewards for the current snapshot
 const txHashes = [
-  '0xc2d3d650696e0f7379422060cfb6d82ccfc25a0d37606e708a12af6d6b853ce5',
-  '0x2e75d0f47ed803112ce1dd35e403b938b2112ad6d07a8d219baae4921631d8e8',
+  '0x17882f20fde2369b1022b785fa40f9767f95bbafa947c3e6b6bb875aac1e7ab5',
 ];
+// const ethTxHash =
+//   '0xcd2b79aac5dab481f993abcddbbeed6162d1bb17b9e7bb917b163207fca738d3';
+
 // Constant transfer event hash used to filter out all ERC20 Transfer events
 const erc20TransferHash =
   '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
@@ -98,6 +107,7 @@ type UserInfo = {
   lockedBalance: BigNumber;
   relockedBalance: BigNumber;
   eligibleBalance: BigNumber;
+  unlockedBalance: BigNumber;
 };
 
 type UserInfoList = {
@@ -127,32 +137,48 @@ async function main() {
     multicallAddress,
     provider
   ) as Multicall;
-  const rewardDistributor = (await ethers.getContractAt(
-    'RewardDistributor',
-    rewardDistributorAddress
-  )) as RewardDistributor;
+  // const rewardDistributor = (await ethers.getContractAt(
+  //   'RewardDistributor',
+  //   rewardDistributorAddress
+  // )) as RewardDistributor;
 
-  const multisigAddress = (await rewardDistributor.MULTISIG()).toLowerCase();
+  // const multisigAddress = (await rewardDistributor.MULTISIG()).toLowerCase();
 
-  // Get unique list of lockers from the very beginning
+  // Get unique list of lockers from the previous deadline
   const lockedEvents = await rlBtrfly.queryFilter(
     rlBtrfly.filters.Locked(),
-    0,
-    minBlock
+    previousBlock,
+    curBlock - 1
   );
+  // console.log(lockedEvents.length);
+
+  // Combine it with the previous lockers from the very beginning
   const tmpLockers: Set<string> = new Set<string>();
   lockedEvents.map(({ args }) => tmpLockers.add(args[0].toLowerCase()));
+
+  if (previousDeadline) {
+    const fileData = await fsReadFileAsync(
+      `${currentSnapshotDir}/${previousDeadline}.json`
+    );
+    const obj = JSON.parse(fileData);
+
+    Object.keys(obj).forEach((account) =>
+      tmpLockers.add(account.toLowerCase())
+    );
+  }
+
   const lockers = Array.from(tmpLockers);
-  console.log(lockers.length);
+  console.log('Total Lockers', lockers.length); // Since the first epoch
 
   // Use multicall to get lockedBalances info for all lockers
   const lockedBalanceCalls: any = [];
   const lockCalls: any = [];
-  const balanceCalls: any = [];
   lockers.forEach((account) => {
     lockedBalanceCalls.push({
       target: rlBtrflyAddress,
-      callData: rlBtrfly.interface.encodeFunctionData('balanceOf', [account]),
+      callData: rlBtrfly.interface.encodeFunctionData('lockedBalances', [
+        account,
+      ]),
     });
     lockCalls.push({
       target: rlBtrflyAddress,
@@ -160,53 +186,20 @@ async function main() {
         account,
       ]),
     });
-    balanceCalls.push({
-      target: rlBtrflyAddress,
-      callData: rlBtrfly.interface.encodeFunctionData('balanceOf', [account]),
-    });
   });
 
-  // Perform standard balance snapshot at the designated snapshot block
+  // Perform standard balance snapshot at the designated minimum snapshot block (1 week before deadline)
   const userInfoList: UserInfoList = {};
+  const cachedSnapshot: any = {};
   const tmpCalls: any = [];
   let idx = 0;
+  console.log(lockedBalanceCalls.length);
   await Promise.each(lockedBalanceCalls, async (row, i) => {
     tmpCalls.push(row);
 
-    if ((i > 0 && i % 100 === 0) || i === lockedBalanceCalls.length - 1) {
-      const encodedData = await multicall.callStatic.aggregate(tmpCalls, {
-        blockTag: minBlock,
-      });
+    if ((i > 0 && i % 50 === 0) || i === lockedBalanceCalls.length - 1) {
+      await Promise.delay(500);
 
-      encodedData.returnData.forEach((returnData) => {
-        const decoded = rlBtrfly.interface.decodeFunctionResult(
-          'balanceOf',
-          returnData
-        );
-
-        const balance = decoded[0];
-
-        userInfoList[lockers[idx]] = {
-          lockedBalance: balance,
-          relockedBalance: BigNumber.from(0),
-          eligibleBalance: balance,
-          locks: [],
-        };
-
-        idx += 1;
-      });
-
-      tmpCalls.length = 0;
-    }
-  });
-
-  // Fetch locks data at the snapshot block to check relocks later
-  tmpCalls.length = 0;
-  idx = 0;
-  await Promise.each(lockCalls, async (row, i) => {
-    tmpCalls.push(row);
-
-    if ((i > 0 && i % 100 === 0) || i === lockCalls.length - 1) {
       const encodedData = await multicall.callStatic.aggregate(tmpCalls, {
         blockTag: minBlock,
       });
@@ -223,46 +216,27 @@ async function main() {
           locks.push({ amount, unlockTime });
         });
 
-        userInfoList[lockers[idx]].locks = locks;
-
-        idx += 1;
-      });
-
-      tmpCalls.length = 0;
-    }
-  });
-
-  // Fetch latest balance to be used for tracking potential relocks
-  tmpCalls.length = 0;
-  idx = 0;
-  await Promise.each(balanceCalls, async (row, i) => {
-    tmpCalls.push(row);
-
-    if ((i > 0 && i % 100 === 0) || i === balanceCalls.length - 1) {
-      const encodedData = await multicall.callStatic.aggregate(tmpCalls, {
-        blockTag: maxBlock,
-      });
-
-      encodedData.returnData.forEach((returnData) => {
-        const decoded = rlBtrfly.interface.decodeFunctionResult(
-          'balanceOf',
-          returnData
-        );
-        const balance = decoded[0];
         let unlockedBalance = BigNumber.from(0);
+        let lockedBalance = BigNumber.from(0);
 
-        userInfoList[lockers[idx]].locks.forEach((lock) => {
+        locks.forEach((lock) => {
           if (lock.unlockTime === deadline) {
             unlockedBalance = lock.amount;
+          } else {
+            lockedBalance = lockedBalance.add(lock.amount);
           }
         });
 
-        userInfoList[lockers[idx]].relockedBalance = unlockedBalance.lt(balance)
-          ? unlockedBalance
-          : balance;
-        userInfoList[lockers[idx]].eligibleBalance = userInfoList[
-          lockers[idx]
-        ].lockedBalance.add(userInfoList[lockers[idx]].relockedBalance);
+        userInfoList[lockers[idx]] = {
+          lockedBalance: lockedBalance,
+          relockedBalance: BigNumber.from(0),
+          eligibleBalance: lockedBalance,
+          unlockedBalance: unlockedBalance,
+          locks: [],
+        };
+
+        cachedSnapshot[lockers[idx]] =
+          userInfoList[lockers[idx]].eligibleBalance.toString();
 
         totalBalance = totalBalance.add(
           userInfoList[lockers[idx]].eligibleBalance
@@ -275,7 +249,84 @@ async function main() {
     }
   });
 
-  console.log('Total Eligible Balance', totalBalance);
+  // Fetch locks data at the snapshot block to check relocks
+  tmpCalls.length = 0;
+  totalBalance = BigNumber.from(0);
+  idx = 0;
+  await Promise.each(lockCalls, async (row, i) => {
+    tmpCalls.push(row);
+
+    if ((i > 0 && i % 100 === 0) || i === lockCalls.length - 1) {
+      await Promise.delay(500);
+
+      const encodedData = await multicall.callStatic.aggregate(tmpCalls, {
+        blockTag: maxBlock,
+      });
+
+      encodedData.returnData.forEach((returnData) => {
+        const decoded = rlBtrfly.interface.decodeFunctionResult(
+          'lockedBalances',
+          returnData
+        );
+
+        const locks: Lock[] = [];
+        decoded[3].forEach((lock: any) => {
+          const [amount, unlockTime] = lock;
+          locks.push({ amount, unlockTime });
+        });
+
+        userInfoList[lockers[idx]].locks = locks;
+
+        // Get the smaller amount between unlocked and newly locked in the grace period
+        let relockedBalance = BigNumber.from(0);
+        let newLockedBalance = BigNumber.from(0);
+
+        userInfoList[lockers[idx]].locks.forEach((lock) => {
+          if (lock.unlockTime === deadline + 604800 + 16 * 604800) {
+            relockedBalance = lock.amount;
+          } else if (lock.unlockTime === deadline + 16 * 604800) {
+            newLockedBalance = lock.amount;
+          }
+        });
+
+        // Add any new lock performed right before the actual snapshot deadline
+        userInfoList[lockers[idx]].lockedBalance =
+          userInfoList[lockers[idx]].lockedBalance.add(newLockedBalance);
+
+        // Check for relocks
+        userInfoList[lockers[idx]].relockedBalance = userInfoList[
+          lockers[idx]
+        ].unlockedBalance.lt(relockedBalance)
+          ? userInfoList[lockers[idx]].unlockedBalance
+          : relockedBalance;
+
+        // Eligible balance should be the sum between curently active lock at snapshot
+        // plus any relock performed for unlocked balance at snapshot within 1 week
+        userInfoList[lockers[idx]].eligibleBalance = userInfoList[
+          lockers[idx]
+        ].lockedBalance.add(userInfoList[lockers[idx]].relockedBalance);
+
+        cachedSnapshot[lockers[idx]] =
+          userInfoList[lockers[idx]].eligibleBalance.toString();
+
+        totalBalance = totalBalance.add(
+          userInfoList[lockers[idx]].eligibleBalance
+        );
+
+        idx += 1;
+      });
+
+      tmpCalls.length = 0;
+    }
+  });
+
+  // Store the snapshotted eligible balances
+  const fileName = `${currentSnapshotDir}/${deadline}.json`;
+
+  await fsMkdirAsync(currentSnapshotDir, { recursive: true });
+  await fsWriteFileAsync(fileName, JSON.stringify(cachedSnapshot));
+
+  console.log('Total Eligible Balance', totalBalance.toString());
 
   const tokenBalances: TokenBalance = {};
 
@@ -284,31 +335,25 @@ async function main() {
     const tx = await provider.getTransaction(txHash);
     const events = await tx.wait();
 
-    // Add native token transfers when available with the multisig address set as the identifier
-    if (tx.value.gt(0)) {
-      if (multisigAddress in tokenBalances) {
-        tokenBalances[multisigAddress] = tokenBalances[multisigAddress].add(
-          tx.value
-        );
-      } else {
-        tokenBalances[multisigAddress] = tx.value;
-      }
-    }
-
     // Loop through all logs and parse all Transfer events
     events.logs.forEach((log) => {
       const token = log.address.toLowerCase();
       const hash = log.topics[0];
-
+      console.log('Topics', log.topics);
       if (hash === erc20TransferHash) {
         const params = new ethers.utils.Interface(erc20Abi).parseLog(log);
         const [from, to, amount] = params.args;
         console.log('Token Transfer', token, from, to, amount);
 
-        if (token in tokenBalances) {
-          tokenBalances[token] = tokenBalances[token].add(amount);
-        } else {
-          tokenBalances[token] = amount;
+        if (
+          (token === wethAddress || token === btrflyV2Address) &&
+          to.toLowerCase() === rewardDistributorAddress.toLowerCase()
+        ) {
+          if (token in tokenBalances) {
+            tokenBalances[token] = tokenBalances[token].add(amount);
+          } else {
+            tokenBalances[token] = amount;
+          }
         }
       }
     });
@@ -318,10 +363,13 @@ async function main() {
 
   // Calculate the reward data based on users' balance ratios for the current snapshot
   const tokenUserRewards: { [token: string]: UserReward } = {};
+  const totalDistributed: any = {};
   Object.keys(tokenBalances).forEach((token) => {
     if (!(token in tokenUserRewards)) {
       tokenUserRewards[token] = {};
     }
+
+    totalDistributed[token] = BigNumber.from(0);
 
     Object.keys(userInfoList).forEach((account) => {
       const { eligibleBalance } = userInfoList[account];
@@ -331,20 +379,24 @@ async function main() {
 
       if (amount.gt(0)) {
         tokenUserRewards[token][account] = amount.toString();
+
+        totalDistributed[token] = totalDistributed[token].add(amount);
       }
     });
   });
 
+  console.log('Distributed', totalDistributed);
+
   // Fetch previous snapshot's reward distribution data
   const skipDistribution: { [token: string]: boolean } = {};
   if (previousDeadline) {
-    const tmpPreviousFiles = await fsReadDirAsync(previousDir);
+    const tmpPreviousFiles = await fsReadDirAsync(previousDistDir);
     const previousFiles = tmpPreviousFiles.filter(
       (file) => path.extname(file) === '.json'
     );
 
     await Promise.each(previousFiles, async (file: any) => {
-      const fileData = await fsReadFileAsync(`${previousDir}/${file}`);
+      const fileData = await fsReadFileAsync(`${previousDistDir}/${file}`);
       const obj = JSON.parse(fileData);
       const token = path.parse(file).name;
 
@@ -421,9 +473,9 @@ async function main() {
       }
     });
 
-    const fileName = `${currentDir}/${token}.json`;
+    const fileName = `${currentDistDir}/${token}.json`;
 
-    await fsMkdirAsync(currentDir, { recursive: true });
+    await fsMkdirAsync(currentDistDir, { recursive: true });
     await fsWriteFileAsync(fileName, JSON.stringify(cachedDistribution));
 
     if (!(token in skipDistribution)) {
@@ -449,6 +501,7 @@ async function main() {
     transformedCallParams.push([token, merkleRoot, proof]);
   });
   console.log('Calldata', transformedCallParams);
+  console.log(Object.keys(claimMetadata).length);
 
   // Update the user reward records
   await Promise.each(Object.keys(claimMetadata), async (account) => {
